660af70359b50e66e541f0c09695d19668d7a9d2
APIVerve Team <hello@apiverve.com>
2025-03-28 17:20:31 -0500

    Bug Fixes

---
+++ src/apiverve_exchangerate/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_exchangerate',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Exchange Rate is a simple tool for getting the current exchange rate between two currencies. It returns the current exchange rate.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
